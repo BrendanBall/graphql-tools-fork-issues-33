296e35f27c3b2ca1f569e2e0318649aec77e32a7
Brendan Ball <brendan@nomanini.com>
2020-01-10 18:45:15 +0200

    reproduction of issue 33

---
+++ src/app.js
@@ -0,0 +1,113 @@
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+  mergeSchemas,
+  delegateToSchema
+} from 'graphql-tools-fork'
+import { graphql } from 'graphql'
+import DataLoader from 'dataloader'
+
+const taskSchema = makeExecutableSchema({
+  typeDefs: `
+    type Task {
+      id: ID!
+      text: String
+      userId: ID!
+    }
+
+    type Query {
+      task(id: ID!): Task
+      tasks: [Task!]!
+      taskByUserId(UserId: ID!): [Task!]!
+    }
+    `
+})
+
+addMockFunctionsToSchema({ schema: taskSchema })
+
+const userSchema = makeExecutableSchema({
+  typeDefs: `
+    interface User {
+      id: ID!
+      email: String!
+    }
+
+    type Editor implements User {
+      id: ID!
+      email: String!
+      name: String!
+    }
+
+    type Admin implements User {
+      id: ID!
+      email: String!
+      godmode: Boolean!
+    }
+
+    type Query {
+      userById(id: ID!): User
+      usersByIds(ids: [ID!]!): [User]!
+    }
+  `
+})
+
+addMockFunctionsToSchema({ schema: userSchema })
+
+const linkTypeDefs = `
+  extend type Task {
+    user: User!
+  }
+`
+
+const resolvers = {
+  Task: {
+    user: {
+      fragment: `... on Task { userId }`,
+      resolve (task, args, context, info) {
+        return context.loaders.users.load({ id: task.userId, info })
+      }
+    }
+  }
+}
+
+export const schema = mergeSchemas({
+  schemas: [
+    taskSchema,
+    userSchema
+  ],
+  typeDefs: linkTypeDefs,
+  resolvers
+})
+
+function dataloaders () {
+  return {
+    users: new DataLoader(async (keys) =>
+      delegateToSchema({
+        schema: userSchema,
+        operation: 'query',
+        fieldName: 'usersByIds',
+        args: {
+          ids: keys.map(k => k.id)
+        },
+        context: null,
+        info: keys[0].info
+      }))
+  }
+}
+
+async function run () {
+  const query = `{
+    task(id: "1") {
+      id
+      text
+      user {
+        id
+        email
+      }
+    }
+  }`
+  const result = await graphql(schema, query, null, { loaders: dataloaders() })
+  console.log('result: %O', result)
+}
+
+run()
